94ae6793b696282a5b24a38de13d0fb7a2f870cb
Piotr Skorupa <35628226+piotreksko@users.noreply.github.com>
2018-11-15 20:41:27 +0100

    Hide cpuPlayer cards

--- src/components/CpuPlayer.js
+++ src/components/CpuPlayer.js
@@ -3,33 +3,33 @@ import Card from "./cards/Card";
 import CardBack from "./cards/CardBack";
 import PropTypes from 'prop-types';
 
-// class CpuPlayer extends Component {
-//   render() {
-//     return (
-//       <div className={"flex-container cards-container"}>
-//         <div className={"row cards"}>
-//           {this.props.cpuPlayer.cards.map((card, index) => (
-//             <CardBack key={index} />
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 class CpuPlayer extends Component {
   render() {
     return (
       <div className={"flex-container cards-container"}>
         <div className={"row cards"}>
-                    {this.props.cpuPlayer.cards.map((card, index) => (
-            <Card key={index} clickOwnCard={this.props.clickOwnCard} card={card} index={index} cardClass={'cardsInHand'} />
+          {this.props.cpuPlayer.cards.map((card, index) => (
+            <CardBack key={index} />
           ))}
         </div>
       </div>
     );
   }
 }
+
+// class CpuPlayer extends Component {
+//   render() {
+//     return (
+//       <div className={"flex-container cards-container"}>
+//         <div className={"row cards"}>
+//                     {this.props.cpuPlayer.cards.map((card, index) => (
+//             <Card key={index} clickOwnCard={this.props.clickOwnCard} card={card} index={index} cardClass={'cardsInHand'} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 CpuPlayer.propTypes = {
   cpuPlayer: PropTypes.shape({
