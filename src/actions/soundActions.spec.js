c1517af1cf4d4c470c316dee6c628e81a79589fe
Piotr Skorupa <35628226+piotreksko@users.noreply.github.com>
2019-02-01 15:06:04 +0100

    Test sounds

---
+++ src/actions/soundActions.spec.js
@@ -0,0 +1,49 @@
+import configureStore from "redux-mock-store"; //ES6 modules
+import thunk from "redux-thunk";
+import { checkSoundsToPlay } from "./soundActions";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+// You would import the action from your codebase in a real scenario
+
+describe("sound actions", () => {
+  const initialState = {};
+  let store;
+  let actions;
+
+  it("should dispatch action", () => {
+    const sound = "ACE";
+    const playSound = sound => ({ type: sound.toUpperCase() });
+    store = mockStore(initialState);
+    store.dispatch(playSound(sound));
+    actions = store.getActions();
+
+    const expectedPayload = { type: sound };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  // describe("check sounds to play", () => {
+  //   const testValues = [
+  //     "chosenType",
+  //     "chosenWeight",
+  //     "waitTurn",
+  //     "battleCardActive"
+  //   ];
+  //   const testSounds = ["jack", "ace", "wait", "battle"];
+  //   let value = true;
+
+  //   testValues.forEach((test, index) => {
+  //     debugger;
+  //     store = mockStore(initialState);
+  //     store.dispatch(checkSoundsToPlay(test, value));
+  //     actions = store.getActions();
+
+  //     it("plays specific sound", () => {
+  //       const expectedPayload = [{ type: testSounds[index].toUpperCase() }];
+  //       const actions = store.getActions();
+  //       expect(actions).toEqual(expectedPayload);
+  //     });
+  //   });
+  // });
+});
